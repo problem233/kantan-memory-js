85032c64bb5e0d88eb257e019ba637b79dfecd7d
problem233 <problem233@outlook.com>
2018-08-09 00:00:17 +0800

    logic

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict'
+
 const app = (() => {
-  const $ = elementId => document.getElementById(elementId),
-        match = (value, mapping) => mapping[value](),
-        toggleHidden = (...elementId) =>
+  const $ = elementId => document.getElementById(elementId)
+  const match = (value, mapping) => mapping[value]()
+  const toggleHidden = (...elementId) =>
     elementId.map($).forEach(elem => {
       if (elem.classList.contains('hidden'))
         elem.classList.remove('hidden')
@@ -38,12 +39,15 @@ const app = (() => {
           $('word-list').children[1].selectedIndex = 0
           $('start').disabled = true
         } else {
-          wordList = res.data.map(elem => ({
-            word: elem[0],
-            kana: elem[1],
-            meaning: elem[2],
-            accent: Number.parseInt(elem[3])
-          }))
+          wordList = {
+            name: file.name ? file.name : file,
+            data: res.data.map(elem => ({
+              word: elem[0],
+              kana: elem[1],
+              meaning: elem[2],
+              accent: Number.parseInt(elem[3])
+            }))
+          }
         }
       }
     })
@@ -63,12 +67,89 @@ const app = (() => {
     }
   }
 
-  let yes = () => alert("impossible action"),
-      no = yes,
-      next = yes
+  const impossibleAct = () => alert("impossible action")
+  const toggleButtons = () =>
+    $('buttons').children.forEach(elem => elem.disabled = !elem.disabled)
+  const makeAccent = word => ((word.accent.toString == 'NaN') 
+    ? "" : "（" + word.accent + "）")
+
+  let yes = impossibleAct,
+      no = impossibleAct,
+      next = impossibleAct
 
   function start () {
-    alert("Warning! The app is still WIP!")
+    let process = Number.parseInt(
+      localStorage['process_' + wordList.name]
+      ? localStorage['process_' + wordList.name]
+      : "0")
+    const incProcess = () => {
+      process++
+      localStorage['process_' + wordList.name] = process.toString()
+    }
+    const userData = JSON.parse(
+      localStorage.userData ? localStorage.userData : "{}")
+    function askKana (wordList) {
+      function showKana (word) {
+        $('kana').innerText = word.kana
+        $('word').innerText = ""
+        $('meaning').innerText = ""
+      }
+      if (!wordList.reviewList.length) {
+        showKana(wordList.reviewList[0].word)
+      } else {
+        showKana(wordList.newWordList[0])
+      }
+      toggleButtons()
+      yes = () => showAnswer(wordList)
+      no = () => askWord(wordList)
+      next = impossibleAct
+    }
+    function askWord (wordList) {
+      function showWord (word) {
+        $('kana').innerText = ""
+        $('word').innerText = word.word + makeAccent(word)
+        $('meaning').innerText = ""
+      }
+      if (!wordList.reviewList.length) {
+        showWord(wordList.reviewList[0].word)
+      } else {
+        showWord(wordList.newWordList[0])
+      }
+      yes = no = () => showAnswer(wordList)
+      next = impossibleAct
+    }
+    function showAnswer (wordList) {
+      function showAnswer_ (word) {
+        $('kana').innerText = word.kana + makeAccent(word)
+        $('word').innerText = word.word
+        $('meaning').innerText = word.meaning
+      }
+      if (!wordList.reviewList.length) {
+        showAnswer_(wordList.reviewList[0].word)
+        if (wordList.reviewList[0].familarity < 2)
+          wordList.reviewList[0].familarity++ // TODO familarity up&downgrade
+      } else {
+        showAnswer_(wordList.newWordList[0])
+      }
+      toggleButtons()
+      yes = no = impossibleAct
+      if (!wordList.reviewList.length)
+        next = () => askKana({
+          reviewList: wordList.reviewList.slice(1),
+          newWordList: wordList.newWordList
+        })
+      else if (!wordList.newWordList.length)
+        next = () => askKana({
+          reviewList: [],
+          newWordList: wordList.newWordList.slice(1)
+        })
+      else next = () => {
+        $('kana').innerText = ""
+        $("word").innerText = "All words are finished!"
+        $("meaning").innerText = ""
+        $('buttons').children[2].disabled = true
+      }
+    }
     match($('mode').children[1].value, {
       'new words': () => {
         // TODO: build word list
@@ -83,5 +164,5 @@ const app = (() => {
   return {
     changeMode,
     changeWordList, getWordList, importWordList, importWordListFile,
-    start, yes, no, continue: next }
+    start, yes: () => yes(), no: () => no(), continue: () => next() }
 })()
