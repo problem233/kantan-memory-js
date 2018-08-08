50c3e187e15322317a0f446e9d90a0ed9f064a87
problem233 <problem233@outlook.com>
2018-08-08 19:33:59 +0800

    wordlist selection

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict'
 const app = (() => {
-  const $ = elementId => document.getElementById(elementId)
-  const match = (value, mapping) => mapping[value]()
-  const toggleHidden = (...elementId) =>
+  const $ = elementId => document.getElementById(elementId),
+        match = (value, mapping) => mapping[value](),
+        toggleHidden = (...elementId) =>
     elementId.map($).forEach(elem => {
       if (elem.classList.contains('hidden'))
         elem.classList.remove('hidden')
       else elem.classList.add('hidden')
     })
+
+  let wordList
 
   function changeMode () {
     toggleHidden('word-list', 'word-number', 'review-number')
@@ -27,22 +29,59 @@ const app = (() => {
     $('start').disabled = false
   }
 
+  function getWordList (download, file) {
+    Papa.parse(file, {
+      download,
+      complete: (res, file) => {
+        if (res.errors.length) {
+          alert("Illegal format!")
+          $('word-list').children[1].selectedIndex = 0
+          $('start').disabled = true
+        } else {
+          wordList = res.data.map(elem => ({
+            word: elem[0],
+            kana: elem[1],
+            meaning: elem[2],
+            accent: Number.parseInt(elem[3])
+          }))
+        }
+      }
+    })
+  }
+
+  function importWordList () {
+    $('get-file').click()
+  }
+
+  function importWordListFile () {
+    const files = $('get-file').files
+    if (files.length) {
+      getWordList(false, files[0])
+    } else {
+      $('word-list').children[1].selectedIndex = 0
+      $('start').disabled = true
+    }
+  }
+
+  let yes = () => alert("impossible action"),
+      no = yes,
+      next = yes
+
   function start () {
     alert("Warning! The app is still WIP!")
+    match($('mode').children[1].value, {
+      'new words': () => {
+        // TODO: build word list
+      },
+      'review': () => {
+
+      }
+    })
     toggleHidden('settings', 'flex', 'buttons')
   }
 
-  function yes () {
-    alert("yes")
-  }
-
-  function no () {
-    alert("no")
-  }
-
-  function next () {
-    alert("next")
-  }
-
-  return { changeMode, changeWordList, start, yes, no, continue: next }
+  return {
+    changeMode,
+    changeWordList, getWordList, importWordList, importWordListFile,
+    start, yes, no, continue: next }
 })()
